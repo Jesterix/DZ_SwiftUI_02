519d0d3468181421f6522b9f4dba9c11c3d074ea
Georgy Khaydenko <gh@van.work>
2020-03-16 10:12:01 +0300

    Initial Commit

---
+++ OtusDZ2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OtusDZ2
+//
+//  Created by Георгий Хайденко on 16.03.2020.
+//  Copyright © 2020 George Khaydenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
