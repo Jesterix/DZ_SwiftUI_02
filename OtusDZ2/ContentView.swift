26764dc91c9598cdfced7787723e795e17ada2c8
Georgy Khaydenko <gh@van.work>
2020-03-16 12:01:38 +0300

    added model for ingredients

--- OtusDZ2/ContentView.swift
+++ OtusDZ2/ContentView.swift
@@ -8,22 +8,29 @@
 
 import SwiftUI
 
+final class AdditionalIngredientListModel: ObservableObject {
+    @Published private(set) var items: [String] = ["avocado",
+                                                   "cucumber",
+                                                   "onions"]
+}
+
 struct ContentView: View {
     @EnvironmentObject var recipesViewModel: RecipesViewModel
+    @EnvironmentObject var viewModel: AdditionalIngredientListModel
 
     @State private var selection = 0
-    var news = ["news", "contacts", "weather"]
 
     var body: some View {
         VStack{
+            Text("Salad with tomato and")
             Picker(selection: $selection, label: Text("News") ) {
-                ForEach(0..<news.count) { index in
-                    Text(self.news[index]).tag(index)
+                ForEach(0..<viewModel.items.count) { index in
+                    Text(self.viewModel.items[index]).tag(index)
                 }
             }
             .pickerStyle(SegmentedPickerStyle())
 
-            Text("Value: \(news[selection])")
+            Text("Value: \(viewModel.items[selection])")
             Spacer()
             RecipeListView()
         }
